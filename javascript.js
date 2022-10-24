b9890f3fd2770a6f3946bbce314c5c6d2fafabd2
LaramieCode <LaramieCode@gmail.com>
2022-10-24 13:18:47 -0500

    add function to addBooksToPage

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let libraryContent = document.querySelector(".libraryContent");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -12,4 +14,36 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(tempBook)
 };
 
-addBookToLibrary("Hobbit", "J.R.R Tolkien", 296, false);
+function addBookToPage() {
+    myLibrary.forEach((book) => {
+        newCard = document.createElement("div");
+        libraryContent.appendChild(newCard);
+        newCard.classList.add("bookCard");
+
+        cardTitle = document.createElement("h1");
+        cardAuthor = document.createElement("h2");
+        cardPages = document.createElement("p");
+        cardRead = document.createElement("p");
+
+        newCard.appendChild(cardTitle);
+        newCard.appendChild(cardAuthor);
+        newCard.appendChild(cardPages);
+        newCard.appendChild(cardRead);
+
+        cardTitle.textContent = book.title;
+        cardAuthor.textContent = book.author;
+        cardPages.textContent = book.pages;
+        if (book.read === true) {
+            cardRead.textContent = "Read"
+            newCard.classList.add("bookHasRead")
+        }
+        if (book.read === false) {
+            cardRead.textContent = "not Read"
+            newCard.classList.add("bookHasRead")
+        }        
+    });
+};
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 296, true);
+
+addBookToPage()
