8863c707d01d00f8d0777ab4c9288d9fd6f10aeb
LaramieCode <LaramieCode@gmail.com>
2022-10-24 12:05:23 -0500

    add function to addBookToLibrary

--- javascript.js
+++ javascript.js
@@ -7,4 +7,9 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
-let Hobbit = new Book("Hobbit", "J.R.R Tolkien", 296, false);
+function addBookToLibrary(title, author, pages, read) {
+    tempBook = new Book(title, author, pages, read);
+    myLibrary.push(tempBook)
+};
+
+addBookToLibrary("Hobbit", "J.R.R Tolkien", 296, false);
