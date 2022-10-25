85f6a0e2063a4ea77cd37c5bf61db6473ddc5a04
LaramieCode <LaramieCode@gmail.com>
2022-10-25 13:10:36 -0500

    add onPage property and refactor addBookToPage function

--- javascript.js
+++ javascript.js
@@ -5,48 +5,53 @@ const submitBook = document.querySelector("#submitBook")
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, onPage) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.onPage = onPage;
 };
 
-function addBookToLibrary(title, author, pages, read) {
-    tempBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, onPage) {
+    tempBook = new Book(title, author, pages, read, onPage);
     myLibrary.push(tempBook)
 };
 
 function addBookToPage() {
 
-    document.querySelectorAll(".bookCard").forEach(function(e) {e.remove()})
-
     myLibrary.forEach((book) => {
-        newCard = document.createElement("div");
-        libraryContent.appendChild(newCard);
-        newCard.classList.add("bookCard");
 
-        cardTitle = document.createElement("h1");
-        cardAuthor = document.createElement("h2");
-        cardPages = document.createElement("p");
-        cardRead = document.createElement("p");
+        if (book.onPage === false) {
 
-        newCard.appendChild(cardTitle);
-        newCard.appendChild(cardAuthor);
-        newCard.appendChild(cardPages);
-        newCard.appendChild(cardRead);
+            book.onPage = true
 
-        cardTitle.textContent = book.title;
-        cardAuthor.textContent = `By: ${book.author}`;
-        cardPages.textContent = `${book.pages} Pages`;
-        if (book.read === true) {
-            cardRead.textContent = "Have Read"
-            newCard.classList.add("bookHasRead")
-        }
-        if (book.read === false) {
-            cardRead.textContent = "Have not Read"
-            newCard.classList.add("bookHasRead")
-        }        
+            newCard = document.createElement("div");
+            libraryContent.appendChild(newCard);
+            newCard.classList.add("bookCard");
+
+            cardTitle = document.createElement("h1");
+            cardAuthor = document.createElement("h2");
+            cardPages = document.createElement("p");
+            cardRead = document.createElement("p");
+
+            newCard.appendChild(cardTitle);
+            newCard.appendChild(cardAuthor);
+            newCard.appendChild(cardPages);
+            newCard.appendChild(cardRead);
+
+            cardTitle.textContent = book.title;
+            cardAuthor.textContent = `By: ${book.author}`;
+            cardPages.textContent = `${book.pages} Pages`;
+            if (book.read === true) {
+                cardRead.textContent = "Have Read"
+                newCard.classList.add("bookHasRead")
+            }
+            if (book.read === false) {
+                cardRead.textContent = "Have not Read"
+                newCard.classList.add("bookHasRead")
+            }
+        }   
     });
 };
 
@@ -58,12 +63,12 @@ addBookBtn.addEventListener("click", function() {
 submitBook.addEventListener("click", function() {
     let formData = document.querySelectorAll(".addBookForm input")
 
-    if (formData[3].checked) {addBookToLibrary(formData[0].value, formData[1].value, formData[2].value, true)}
-    if (formData[4].checked) {addBookToLibrary(formData[0].value, formData[1].value, formData[2].value, false)}
+    if (formData[3].checked) {addBookToLibrary(formData[0].value, formData[1].value, formData[2].value, true, false)}
+    if (formData[4].checked) {addBookToLibrary(formData[0].value, formData[1].value, formData[2].value, false, false)}
 
     addBookToPage()
 });
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", "296", true);
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "296", true, false);
 
 addBookToPage()
