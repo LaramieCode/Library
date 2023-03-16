364cfc6cfeaa80926fe1721d6c8cc09dc2ed5bf4
LaramieCode <LaramieCode@gmail.com>
2023-03-16 17:17:35 -0500

    refactor using classes

--- javascript.js
+++ javascript.js
@@ -1,113 +1,133 @@
-let libraryContent = document.querySelector(".libraryContent");
+const libraryContent = document.querySelector(".libraryContent");
 const addBookBtn = document.querySelector(".addBook");
-const bookFormContainer = document.querySelector(".formContainer")
-const submitBook = document.querySelector("#submitBook")
+const bookFormContainer = document.querySelector(".formContainer");
+const submitBook = document.querySelector("#submitBook");
 
-let myLibrary = [];
+addBookBtn.addEventListener("click", function () {
+  bookFormContainer.classList.toggle("dontDisplay");
+  libraryContent.classList.toggle("dontDisplay");
+});
 
-function Book(title, author, pages, read, onPage) {
+submitBook.addEventListener("click", () => {
+  let formData = document.querySelectorAll(".addBookForm input");
+  let newBook;
+
+  if (formData[3].checked) {
+    newBook = new Book(
+      formData[0].value,
+      formData[1].value,
+      formData[2].value,
+      true,
+      false
+    );
+  }
+  if (formData[4].checked) {
+    newBook = new Book(
+      formData[0].value,
+      formData[1].value,
+      formData[2].value,
+      false,
+      false
+    );
+  }
+  newBook.addBookToLibrary();
+  Book.updatePage();
+});
+
+class Book {
+  static library = [];
+
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.onPage = onPage;
-};
+  }
 
-function addBookToLibrary(title, author, pages, read, onPage) {
-    tempBook = new Book(title, author, pages, read, onPage);
-    myLibrary.push(tempBook)
-};
+  addBookToLibrary() {
+    Book.library.push(this);
+  }
+  removeBookFromLibrary() {
+    let index = this.locateLibraryIndex();
+    Book.library.splice(index, 1);
+  }
+  locateLibraryIndex() {
+    for (let i = Book.library.length; i > 0; i--) {
+      if (this == Book.library[i - 1]) {
+        return i - 1;
+      }
+    }
+  }
 
-function addBookToPage() {
-
-    myLibrary.forEach((book) => {
-
-        if (book.onPage === false) {
-
-            book.onPage = true
-
-            let newCard = document.createElement("div");
-            libraryContent.appendChild(newCard);
-            newCard.classList.add("bookCard");
-
-            let cardTitle = document.createElement("h1");
-            let cardAuthor = document.createElement("h2");
-            let cardPages = document.createElement("p");
-            let cardRead = document.createElement("p");
-            let cardDelete = document.createElement("button");
-
-            newCard.appendChild(cardTitle);
-            newCard.appendChild(cardAuthor);
-            newCard.appendChild(cardPages);
-            newCard.appendChild(cardRead);
-            newCard.appendChild(cardDelete)
-
-            cardDelete.classList.add("delete")
-
-            cardTitle.textContent = book.title;
-            cardAuthor.textContent = `By: ${book.author}`;
-            cardPages.textContent = `${book.pages} Pages`;
-            if (book.read === true) {
-                cardRead.textContent = "Have Read"
-                newCard.classList.add("bookHasRead")
-            }
-            if (book.read === false) {
-                cardRead.textContent = "Have not Read"
-                newCard.classList.add("bookHasNotRead")
-            }
-            cardDelete.textContent = "Remove"
-
-            cardDelete.addEventListener("click", function(){
-                removeBookFromLibrary(book);
-            });
-
-            if (book.read === false) {
-                cardNowRead = document.createElement("button");
-                newCard.appendChild(cardNowRead);
-                cardNowRead.classList.add("readToggle")
-                cardNowRead.textContent = "Read yet?"
-
-                cardNowRead.addEventListener("click", () => {
-                    console.log("hey")
-                    newCard.classList.remove("bookHasNotRead")
-                    newCard.classList.add("bookHasRead")
-
-                    cardNowRead.classList.add("dontDisplay")
-                })
-            }
-        }   
+  static clearPage() {
+    let content = document.querySelectorAll(".bookCard");
+    content.forEach((card) => {
+      card.remove();
     });
-};
+  }
 
-function removeBookFromLibrary(book) {
-    let match = myLibrary.findIndex( (value) => {
-        return value === book
-    })
-    myLibrary.splice(match, 1)
+  static updatePage() {
+    Book.clearPage();
 
-    removeBookFromPage(match)
+    Book.library.forEach((book) => {
+      let newCard = document.createElement("div");
+      libraryContent.appendChild(newCard);
+      newCard.classList.add("bookCard");
+
+      let cardTitle = document.createElement("h1");
+      let cardAuthor = document.createElement("h2");
+      let cardPages = document.createElement("p");
+      let cardRead = document.createElement("p");
+      let cardDelete = document.createElement("button");
+
+      newCard.appendChild(cardTitle);
+      newCard.appendChild(cardAuthor);
+      newCard.appendChild(cardPages);
+      newCard.appendChild(cardRead);
+      newCard.appendChild(cardDelete);
+
+      cardDelete.classList.add("delete");
+
+      cardTitle.textContent = book.title;
+      cardAuthor.textContent = `By: ${book.author}`;
+      cardPages.textContent = `${book.pages} Pages`;
+      if (book.read === true) {
+        cardRead.textContent = "Have Read";
+        newCard.classList.add("bookHasRead");
+      }
+      if (book.read === false) {
+        cardRead.textContent = "Have not Read";
+        newCard.classList.add("bookHasNotRead");
+      }
+      cardDelete.textContent = "Remove";
+
+      cardDelete.addEventListener("click", function () {
+        book.removeBookFromLibrary();
+        Book.updatePage();
+      });
+
+      if (book.read === false) {
+        let cardNowRead = document.createElement("button");
+        newCard.appendChild(cardNowRead);
+        cardNowRead.classList.add("readToggle");
+        cardNowRead.textContent = "Read yet?";
+
+        cardNowRead.addEventListener("click", () => {
+          newCard.classList.remove("bookHasNotRead");
+          newCard.classList.add("bookHasRead");
+
+          cardNowRead.classList.add("dontDisplay");
+        });
+      }
+    });
+  }
 }
 
-function removeBookFromPage(match) {
-    pageBooks = document.querySelectorAll(".bookCard")
-    pageBooks[match].remove()
-}
+let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "296", true);
+theHobbit.addBookToLibrary();
+let cat = new Book("The Cat in the Hat", "Dr. Seuss", "61", false);
+cat.addBookToLibrary();
+let sherlock = new Book("sherlock", "holmes", "255", true);
+sherlock.addBookToLibrary();
 
-addBookBtn.addEventListener("click", function() {
-    bookFormContainer.classList.toggle("dontDisplay")
-    libraryContent.classList.toggle("dontDisplay")
-});
-
-submitBook.addEventListener("click", function() {
-    let formData = document.querySelectorAll(".addBookForm input")
-
-    if (formData[3].checked) {addBookToLibrary(formData[0].value, formData[1].value, formData[2].value, true, false)}
-    if (formData[4].checked) {addBookToLibrary(formData[0].value, formData[1].value, formData[2].value, false, false)}
-
-    addBookToPage()
-});
-
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", "296", true, false);
-addBookToLibrary("The Cat in the Hat", "Dr. Seuss", "61", false, false)
-
-addBookToPage()
+Book.updatePage();
